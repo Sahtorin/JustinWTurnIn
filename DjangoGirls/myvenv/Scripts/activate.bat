186a47f94a4286a2db0d268e500ba333fd131eec
Your Name <you@example.com>
2026-04-15 11:03:06 -0400

    redo

--- DjangoGirls/myvenv/Scripts/activate.bat
+++ DjangoGirls/myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\13179\OneDrive\Attachments\Desktop\BookApi\myvenv"
+set "VIRTUAL_ENV=C:\Users\13179\OneDrive\Attachments\Desktop\Mod4tut\myvenv"
 
 if not defined PROMPT set PROMPT=$P$G
 
--- DjangoGirls/myvenv/Scripts/activate.fish
+++ DjangoGirls/myvenv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\13179\OneDrive\Attachments\Desktop\BookApi\myvenv'
+set -gx VIRTUAL_ENV 'C:\Users\13179\OneDrive\Attachments\Desktop\Mod4tut\myvenv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
